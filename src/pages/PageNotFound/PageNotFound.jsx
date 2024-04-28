19665330bbca08c5512d603c6eb5d9501b1e6308
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-04-28 17:43:53 +0300

    `PageNotFound.jsx` modified and added redirect to Home Page

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
-  return <h1 className="mt-lg-5 text-center">Page not found</h1>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      navigate("/");
+    }, 10000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+  return (
+    <Box
+      sx={{ mt: 15 }}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Typography variant="h5" component="div">
+        Ooops....Page not found
+      </Typography>
+      <Typography variant="body2" component="div">
+        The page you are attempting to reach does not exist. Please verify the
+        URL and try again.
+      </Typography>
+      <Typography variant="body2" component="div">
+        You will be redirected to Home page after 10 sec....
+      </Typography>
+    </Box>
+  );
 };
 
 export default PageNotFound;
